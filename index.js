bf487490afd2a21d082316422f771c501629db47
AbbasAyda <84493447+AbbasAyda@users.noreply.github.com>
2022-01-29 12:42:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const http = require("http");
+const express = require('express');
+const socketio=require('socket.io')
+const app = express();
 const server = new http.Server(app);
 const io = socketio(server);
 io.path('/route');
-const express = require('express');
 const mongodb      = require('mongodb');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || '3000';
-const app = express();	
 app.set("port",PORT);
 app.listen(PORT,()=>{
   console.log(`Server listening on ${PORT}`);
     });
-    const server = require('http').createServer();
-const io = require('socket.io')(server);
-io.on('connection', client => {
-  client.on('event', data => { /* … */ });
-  client.on('disconnect', () => { /* … */ });
-});
-server.listen(3000);
