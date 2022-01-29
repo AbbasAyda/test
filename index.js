5bef3a30e3040b4fa3c27f877694c2e7e3071076
AbbasAyda <84493447+AbbasAyda@users.noreply.github.com>
2022-01-29 11:47:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const http = require("http");
 const express = require('express');
 const app = express();	
-const mysql      = require('mysql');
-var bodyParser = require('body-parser');
-const PORT = process.env.PORT | 6565;
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
 console.log(`Server listening on ${PORT}`);
   }); 
