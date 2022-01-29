7342e13f2ffab30a905002ca57a3f52c5da9cbe0
AbbasAyda <84493447+AbbasAyda@users.noreply.github.com>
2022-01-29 11:59:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const http = require("http");
 const express = require('express');
 const app = express();	
-const mongodb = require('mongodb');
+const mongodb      = require('mongodb');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
 console.log(`Server listening on ${PORT}`);
   }); 
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+  app.get('*', (request, response) => {
+    response.write("hello it is ",()=>{})
+  });
