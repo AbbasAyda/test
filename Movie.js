e26ccaf6c95a2c71bde039e1fd253d7a645e1867
AbbasAyda <84493447+AbbasAyda@users.noreply.github.com>
2022-01-29 15:18:13 +0530

    Update Movie.js

--- Movie.js
+++ Movie.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 console.clear()
 
 // Creating a Database
-mongoose.connect('mongodb://localhost/MOVIELIBRARY', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('http://127.0.0.1:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...', err));
 
